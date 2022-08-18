dc28fed6d311e0f2b5130ce3a7c9e7be030d2173
andresmarpz <andresmarpz@gmail.com>
2022-08-18 15:40:32 -0300

    Migrate router to Navigo

--- src/js/init.js
+++ src/js/init.js
@@ -20,13 +20,13 @@ let maxCount = undefined;
 let productCost = 0;
 let productCount = 0;
 let comissionPercentage = 0.13;
-let MONEY_SYMBOL = "$";
-let DOLLAR_CURRENCY = "Dólares (USD)";
-let PESO_CURRENCY = "Pesos Uruguayos (UYU)";
-let DOLLAR_SYMBOL = "USD ";
-let PESO_SYMBOL = "UYU ";
+let MONEY_SYMBOL = '$';
+let DOLLAR_CURRENCY = 'Dólares (USD)';
+let PESO_CURRENCY = 'Pesos Uruguayos (UYU)';
+let DOLLAR_SYMBOL = 'USD ';
+let PESO_SYMBOL = 'UYU ';
 let PERCENTAGE_SYMBOL = '%';
-let MSG = "FUNCIONALIDAD NO IMPLEMENTADA";
+let MSG = 'FUNCIONALIDAD NO IMPLEMENTADA';
 
 const fetchEndpoint = async (url) => {
     return await fetch(url)
@@ -51,3 +51,72 @@ const fetchEndpoint = async (url) => {
 const getImagePath = (url = '') => {
     return url.replace('.jpg', '.webp').replace('.png', '.webp');
 };
+
+/**
+ * Create a Navigo router and configure it
+ */
+
+const routes = {
+    '/': {
+        view: '../views/home.html'
+    },
+    '/categories': {
+        view: '../views/categories.html',
+        scripts: ['../js/categories.js']
+    },
+    '/products': {
+        view: '../views/products.html',
+        scripts: ['../js/products.js']
+    },
+    '/sell': {
+        view: '../views/sell.html',
+        scripts: ['../js/dropzone.js', '../js/sell.js']
+    }
+};
+
+const render = async (path) => {
+    const container = document.querySelector('[navigo-container]');
+    const route = routes[path];
+    if (!route.rendered) {
+        const content = await fetch(route.view).then((response) => response.text());
+		routes[path].rendered = true;
+		routes[path].view = content;
+        container.innerHTML = content;
+    }else container.innerHTML = route.view;
+
+    if (route.scripts) {
+        for (let script of route.scripts) {
+            const scriptElement = document.createElement('script');
+            const content = await fetch(script).then((response) => response.text());
+            if (!content) return;
+            scriptElement.innerHTML = content;
+            container.appendChild(scriptElement);
+        }
+    }
+};
+
+const router = new Navigo('/');
+router.on({
+        '': () => {
+            render('/');
+        },
+        '/categories': () => {
+            render('/categories');
+        },
+        '/products': () => {
+			render('/products');
+		},
+        '/product/:id': (params) => {},
+        '/cart': () => {},
+        '/sell': () => {
+            render('/sell');
+        },
+        '/login': () => {
+            render('/login');
+        },
+        '/index.html': () => {
+            router.navigate('');
+        },
+        '/categories.html': () => {},
+        '/products.html': () => {}
+}).resolve();
