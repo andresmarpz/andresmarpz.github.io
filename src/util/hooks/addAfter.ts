345b1e2e2eb62e302b705e12cdfc4d6210fd3408
andresmarpz <andresmarpz@gmail.com>
2022-09-04 00:51:22 -0300

    add: addBefore hook

--- src/util/hooks/addAfter.ts
+++ src/util/hooks/addAfter.ts
@@ -1,9 +1,10 @@
-import { router } from "../../router";
-import { hooks } from "../store";
+import { router } from '../../router';
+import { hooks } from '../store';
 
-export default function addAfterHook(path: string, fn: () => void){
-	if(hooks.includes(path)) return;
+export default function addAfterHook(path: string, fn: () => void) {
+    const { after } = hooks;
+    if (after.includes(path)) return;
 
-	hooks.push(path);
-	router.addAfterHook(path, fn);
+    after.push(path);
+    router.addAfterHook(path, fn);
 }
