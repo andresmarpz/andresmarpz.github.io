46b93d0179a033f9fe7020c7f5379a780ea671fc
andresmarpz <andresmarpz@gmail.com>
2022-09-04 12:20:18 -0300

    fix: hide navbar on /login

--- src/router.ts
+++ src/router.ts
@@ -21,53 +21,59 @@ export default function createRoutes() {
         { path: '/login', page: Login },
         { path: '/categories', page: Categories },
         { path: '/products', page: Products },
-		{ path: '/product', page: Product },
+        { path: '/product', page: Product },
         { path: '/sell', page: Sell }
     ];
 
-	router.hooks({
-		before: (done, match) => {
-			if (match.url === 'login') {
-				if (localStorage.getItem('profile')) {
-					done(false);
-					router.navigate('/');
-				} else done();
-			} else {
-				if (!localStorage.getItem('profile')) {
-					done(false);
-					localStorage.setItem('lastUrl', match.url + (match.queryString ? '?' + match.queryString : ''));
-					router.navigate('/login');
-				} else done();
-			}
-		},
-		after: () => {
-			const user = document.getElementById('dropdownMenuButton');
-			if (user && localStorage.getItem('profile')) {
-				const profile = JSON.parse(localStorage.getItem('profile')!);
-				user.innerHTML = profile.email;
-			}
-		}
-	});
+    router.hooks({
+        before: (done, match) => {
+            if (match.url === 'login') {
+                if (localStorage.getItem('profile')) {
+                    done(false);
+                    router.navigate('/');
+                } else {
+                    document.getElementById('navbar')!.style.display = 'none';
+                    done();
+                }
+            } else {
+                if (!localStorage.getItem('profile')) {
+                    done(false);
+                    localStorage.setItem('lastUrl', match.url + (match.queryString ? '?' + match.queryString : ''));
+                    router.navigate('/login');
+                } else {
+					document.getElementById('navbar')!.style.display = 'block';
+                    done();
+                }
+            }
+        },
+        after: () => {
+            const user = document.getElementById('dropdownMenuButton');
+            if (user && localStorage.getItem('profile')) {
+                const profile = JSON.parse(localStorage.getItem('profile')!);
+                user.innerHTML = profile.email;
+            }
+        }
+    });
     routes.forEach(({ path, page }) => {
         router
             .on(path, async () => {
                 const container = document.querySelector('[navigo-container]');
                 if (container) container.innerHTML = await page(path);
 
-				// Custom hook running after page load since Navigo's doesn't really
-				// work as expected, calling the after hook before the page is loaded completely
-				const { after } = hooks;
-				const hook = after.find((hook) => hook.path === path);
-				if(hook) hook.fn()
+                // Custom hook running after page load since Navigo's doesn't really
+                // work as expected, calling the after hook before the page is loaded completely
+                const { after } = hooks;
+                const hook = after.find((hook) => hook.path === path);
+                if (hook) hook.fn();
             })
             .resolve();
-	});
-	router.addLeaveHook('/login', (done: (bool?: boolean) => void) => {
-		if (!localStorage.getItem('profile')) done(false);
-		else {
-			const navbar = document.getElementById('navbar');
-			if (navbar) navbar.style.display = 'block';
-			done();
-		}
-	});
+    });
+    router.addLeaveHook('/login', (done: (bool?: boolean) => void) => {
+        if (!localStorage.getItem('profile')) done(false);
+        else {
+            const navbar = document.getElementById('navbar');
+            if (navbar) navbar.style.display = 'block';
+            done();
+        }
+    });
 }
