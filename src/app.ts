5dd5a19fff96e2578a754ba0c7cc898e8b5a26dc
Andrés Martínez <andresmarpz@gmail.com>
2022-10-15 17:57:23 -0300

    fix: dropdown not closing because of data-navigo on anchors

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,17 @@
-import createRoutes, { router } from "./router";
-import 'bootstrap'
+import createRoutes, { router } from './router';
+import 'bootstrap';
 
-createRoutes();
+function setup() {
+  createRoutes();
+
+  document.getElementById('href-cart')?.addEventListener('click', () => router.navigate('/cart'));
+  document.getElementById('href-profile')?.addEventListener('click', () => router.navigate('/profile'));
+}
+
+setup();
 
 // @ts-ignore
-window.salir = function salir(){
-	localStorage.removeItem('profile');
-    router.navigate('/login');
-}
+window.salir = function salir() {
+  localStorage.removeItem('profile');
+  router.navigate('/login');
+};
--- src/router.ts
+++ src/router.ts
@@ -13,71 +13,71 @@ import { hooks } from './lib/store';
 export const router = new Navigo('/');
 
 export interface Route {
-    path: string;
-    page: Page;
+  path: string;
+  page: Page;
 }
 
 export default function createRoutes() {
-    const routes: Route[] = [
-        { path: '/', page: Home },
-        { path: '/login', page: Login },
-        { path: '/categories', page: Categories },
-        { path: '/products', page: Products },
-        { path: '/product', page: Product },
-        { path: '/sell', page: Sell },
-				{ path: '/cart', page: Cart },
-				{ path: '/profile', page: Profile }
-    ];
+  const routes: Route[] = [
+    { path: '/', page: Home },
+    { path: '/login', page: Login },
+    { path: '/categories', page: Categories },
+    { path: '/products', page: Products },
+    { path: '/product', page: Product },
+    { path: '/sell', page: Sell },
+    { path: '/cart', page: Cart },
+    { path: '/profile', page: Profile }
+  ];
 
-    router.hooks({
-        before: (done, match) => {
-            if (match.url === 'login') {
-                if (localStorage.getItem('profile')) {
-                    done(false);
-                    router.navigate('/');
-                } else {
-                    document.getElementById('navbar')!.style.display = 'none';
-                    done();
-                }
-            } else {
-                if (!localStorage.getItem('profile')) {
-                    done(false);
-                    localStorage.setItem('lastUrl', match.url + (match.queryString ? '?' + match.queryString : ''));
-                    router.navigate('/login');
-                } else {
-					document.getElementById('navbar')!.style.display = 'block';
-                    done();
-                }
-            }
-        },
-        after: () => {
-            const user = document.getElementById('dropdownMenuButton');
-            if (user && localStorage.getItem('profile')) {
-                const profile = JSON.parse(localStorage.getItem('profile')!);
-                user.innerHTML = profile.email;
-            }
+  router.hooks({
+    before: (done, match) => {
+      if (match.url === 'login') {
+        if (localStorage.getItem('profile')) {
+          done(false);
+          router.navigate('/');
+        } else {
+          document.getElementById('navbar')!.style.display = 'none';
+          done();
         }
-    });
-    routes.forEach(({ path, page }) => {
-        router
-            .on(path, async () => {
-                const container = document.querySelector('[navigo-container]');
-                if (container) container.innerHTML = await page(path);
+      } else {
+        if (!localStorage.getItem('profile')) {
+          done(false);
+          localStorage.setItem('lastUrl', match.url + (match.queryString ? '?' + match.queryString : ''));
+          router.navigate('/login');
+        } else {
+          document.getElementById('navbar')!.style.display = 'block';
+          done();
+        }
+      }
+    },
+    after: () => {
+      const user = document.getElementById('nav-dropdown-tg');
+      if (user && localStorage.getItem('profile')) {
+        const profile = JSON.parse(localStorage.getItem('profile')!);
+        user.innerHTML = profile.email;
+      }
+    }
+  });
+  routes.forEach(({ path, page }) => {
+    router
+      .on(path, async () => {
+        const container = document.querySelector('[navigo-container]');
+        if (container) container.innerHTML = await page(path);
 
-                // Custom hook running after page load since Navigo's doesn't really
-                // work as expected, calling the after hook before the page is loaded completely
-                const { after } = hooks;
-                const hook = after.find((hook) => hook.path === path);
-                if (hook) hook.fn();
-            })
-            .resolve();
-    });
-    router.addLeaveHook('/login', (done: (bool?: boolean) => void) => {
-        if (!localStorage.getItem('profile')) done(false);
-        else {
-            const navbar = document.getElementById('navbar');
-            if (navbar) navbar.style.display = 'block';
-            done();
-        }
-    });
+        // Custom hook running after page load since Navigo's doesn't really
+        // work as expected, calling the after hook before the page is loaded completely
+        const { after } = hooks;
+        const hook = after.find((hook) => hook.path === path);
+        if (hook) hook.fn();
+      })
+      .resolve();
+  });
+  router.addLeaveHook('/login', (done: (bool?: boolean) => void) => {
+    if (!localStorage.getItem('profile')) done(false);
+    else {
+      const navbar = document.getElementById('navbar');
+      if (navbar) navbar.style.display = 'block';
+      done();
+    }
+  });
 }
