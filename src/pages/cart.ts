0c797680c25fbe70b23ad09703074eb64defc504
Andrés Martínez <andresmarpz@gmail.com>
2022-10-30 20:33:54 -0300

    Entrega 6

--- src/pages/cart.ts
+++ src/pages/cart.ts
@@ -39,6 +39,7 @@ function convertPriceToCurrency(price: number, from: currencyType, to: currencyT
 
 type currencyType = 'USD' | 'UYU';
 type deliveryType = 'free' | 'express' | 'turbo';
+type paymentType = 'card' | 'bank' | undefined;
 
 // State
 let delivery: deliveryType = 'free';
@@ -57,6 +58,12 @@ function setCurrency(currencyType: currencyType) {
   document.getElementById('mon-' + currencyType.toLocaleLowerCase())?.classList.add('dy-selected');
   renderProducts(useStore().cart);
   updateSummary();
+}
+
+let payment: paymentType = undefined;
+function setPaymentMethod(paymentMethod: paymentType){
+	payment = paymentMethod;
+	document.getElementById('payment-method')!.innerHTML = (payment === 'bank' ? 'Transferencia bancaria' : 'Tarjeta de crédito')
 }
 
 const getFee = (delivery: deliveryType) => {
@@ -116,7 +123,7 @@ const renderProducts = (products: ReadonlyArray<CartProduct>) => {
     }
     cart.innerHTML = products
       .map(
-        (prod) => `
+        (prod) => /*html*/`
 						<hr class="cart-separator" />
 						<div class="d-flex flex-nowrap gap-3 py-3 align-items-center">
 								<div class="ci-img">
@@ -155,6 +162,34 @@ const renderProducts = (products: ReadonlyArray<CartProduct>) => {
   }
 };
 
+function disableInputs(){
+	document.getElementById(payment === 'bank' ? 'card-set' : 'bank-set')?.setAttribute('disabled', '');
+	document.getElementById(payment === 'bank' ? 'bank-set' : 'card-set')?.removeAttribute('disabled');
+}
+
+function isFieldsetValid(){
+	const inputs = document.querySelectorAll('fieldset:not([disabled]) input');
+	return Array.from(inputs).every(el => (el as HTMLInputElement).checkValidity())
+}
+
+function updatePaymentValidity(){
+	const btn = document.getElementById('payment-btn');
+	isFieldsetValid() ? btn?.classList.remove('is-invalid') : btn?.classList.add('is-invalid')
+}
+
+function processPurchase(){
+	document.getElementById('alert-container')!.innerHTML = `
+		<div id='alert-add' class="alert alert-success alert-dismissible fade show" style="z-index: 99;" role="alert">
+			<span>¡La compra ha sido realizada con éxito!</span>
+		</div>
+	`
+
+	setTimeout(() => {
+		localStorage.removeItem('cart');
+		window.location.href = '/cart';
+	}, 2000)
+}
+
 const Cart: Page = async (path) => {
   addAfterHook(path, () => {
     const store = useStore();
@@ -168,10 +203,35 @@ const Cart: Page = async (path) => {
     document.getElementById('dy-free')?.addEventListener('click', () => setDelivery('free'));
     document.getElementById('dy-express')?.addEventListener('click', () => setDelivery('express'));
     document.getElementById('dy-turbo')?.addEventListener('click', () => setDelivery('turbo'));
+
+		const form = document.getElementById('cart-form') as HTMLFormElement;
+		form?.addEventListener('submit', (event) => {
+			if(form.checkValidity() && isFieldsetValid()) processPurchase();
+
+			event.preventDefault();
+			event.stopPropagation();
+
+			updatePaymentValidity();
+
+			form.classList.add('was-validated');
+		})
+
+		document.getElementById('card-radio')?.addEventListener('change', () => {
+			setPaymentMethod('card')
+			disableInputs()
+		})
+
+		document.getElementById('bank-radio')?.addEventListener('change', () => {
+			setPaymentMethod('bank')
+			disableInputs()
+		})
+
+		document.querySelectorAll('fieldset input').forEach(el => el.addEventListener('input', updatePaymentValidity))
   });
 
   return /*html*/ `
 			<div class="container mt-4 px-4">
+				<div id="alert-container"></div>
 				<div class="row gap-3">
 					<div class="cart-cnt px-3 col-lg-7 col-xl-8 h-fit-content">
 						<div class="my-2">
@@ -180,52 +240,155 @@ const Cart: Page = async (path) => {
 						<div id="cart"></div>
 					</div>
 					<div class="cart-cnt col h-fit-content">
-						<div class="mt-2">
-							<strong>Moneda</strong>
+						<form class="needs-validation" id="cart-form" novalidate>
+							<div class="mt-2">
+								<strong>Moneda</strong>
 
-							<div class="dy-cnt d-flex mt-2" id="moneda">
-								<button class="mon-btn dy-selected" id='mon-uyu'>Pesos Uruguayos</button>
-								<button class="mon-btn" id='mon-usd'>Dólares</button>
-							</div>
-						</div>
-						<div class="py-4">
-							<strong>Envío</strong>
-							
-							<div class='dy-cnt d-flex my-2' id='delivery'>
-								<button class="dy-btn dy-selected" id='dy-free'>Gratis</button>
-								<button class="dy-btn" id='dy-express'>Express (3%)</button>
-								<button class="dy-btn" id='dy-turbo'>Turbo (7%)</button>
-							</div>
-							
-							<div class='dy-date' id='dy-date'>
-								Fecha estimada: --/--/----
-							</div>
-							
-							<hr class="cart-separator my-2" />
-
-							<div>
-								<div class="d-flex justify-content-between dy-subtotal">
-									<label name="subtotal" for="subtotal">Subtotal: </label>
-									<span id="dy-subtotal">$ --</span>
+								<div class="dy-cnt d-flex mt-2" id="moneda">
+									<button class="mon-btn dy-selected" id='mon-uyu'>Pesos Uruguayos</button>
+									<button class="mon-btn" id='mon-usd'>Dólares</button>
 								</div>
-								<div class="d-flex justify-content-between dy-envio">
-									<label name="envio" for="envio">Envío: </label>
-									<span id="dy-envio">$ --</span>
+							</div>
+							<div class="py-4">
+								<strong>Envío</strong>
+								
+								<div class='dy-cnt d-flex my-2' id='delivery'>
+									<button class="dy-btn dy-selected" id='dy-free'>Gratis</button>
+									<button class="dy-btn" id='dy-express'>Express (3%)</button>
+									<button class="dy-btn" id='dy-turbo'>Turbo (7%)</button>
+								</div>
+								
+								<div class='dy-date' id='dy-date'>
+									Fecha estimada: --/--/----
 								</div>
 
+								<div class="mt-4">
+									<label for="input-calle" class="form-label">Calle</label>
+									<input type="text" class="form-control" id="input-calle" required>
+									<div class="invalid-feedback">
+										Debe especificar calle
+									</div>
+								</div>
+								<div>
+									<label for="input-numero" class="form-label">Número</label>
+									<input type="text" class="form-control" id="input-numero" required>
+									<div class="invalid-feedback">
+										Debe especificar número
+									</div>
+								</div>
+								<div>
+									<label for="input-esquina" class="form-label">Esquina</label>
+									<input type="text" class="form-control" id="input-esquina" required>
+									<div class="invalid-feedback">
+										Debe especificar esquina
+									</div>
+								</div>
+								
+								<hr class="cart-separator my-2" />
+								
+								<div class="py-4">
+									<strong>Forma de pago</strong>
+
+									<div>
+										<div>
+											<span id="payment-method">Tarjeta de crédito</span>
+											<!-- Button trigger modal -->
+											<button id="payment-btn" type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+												Seleccionar
+											</button>
+											<span class="invalid-feedback">Debe seleccionar una forma de pago.</span>
+										</div>
+
+										<!-- Modal -->
+										<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+											<div class="modal-dialog">
+												<div class="modal-content">
+													<div class="modal-header">
+														<h5 class="modal-title" id="exampleModalLabel">Forma de pago</h5>
+														<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+													</div>
+													<div class="modal-body">
+														<div class="form-check payment-method-radio">
+															<input type="radio" class="form-check-input" name="methodRadio" id="card-radio" checked>
+															<label for="card-radio">Tarjeta de credito</label>
+														</div>
+														<fieldset form="cartForm" id="card-set">
+															<div class="row mb-3">
+																<label for="cardNumber" class="form-label">Número de tarjeta</label>
+																<input type="text" class="form-control" id="cardNumber"
+																	minlength="16"
+																	maxlength="16"
+																	pattern="^[0-9]*$"
+																	placeholder="XXXX XXXX XXXX XXXX"
+																	required>
+															</div>
+															<div class="row mb-3">
+																<label for="cvvNumber" class="form-label">Código de seguridad</label>
+																<input type="text" class="form-control" id="cvvNumber"
+																	minlength="3"
+																	maxlength="3"
+																	pattern="^[0-9]*$"
+																	required>
+															</div>
+															<div class="row mb-3">
+																<label for="expiringDate" class="form-label">Vencimiento</label>
+																<input type="text" class="form-control" id="expiringDate"
+																	minlength="5"
+																	maxlength="5"
+																	pattern="[0-9]{2}\/[0-9]{2}"
+																	placeholder="MM/AA"
+																	required>
+															</div>
+														</fieldset>
+														<hr>
+														<div class="form-check payment-method-radio">
+															<input type="radio" class="form-check-input" name="methodRadio" id="bank-radio">
+															<label for="bank-radio">Transferencia bancaria</label>
+														</div>
+														<fieldset form="cartForm" id="bank-set" disabled>
+															<div class="row mb-3">
+																<label for="accountNumber" class="form-label">Número de cuenta</label>
+																<input type="text" class="form-control" id="accountNumber"
+																	pattern="[0-9]*"
+																	required>
+															</div>
+														</fieldset>
+													</div>
+													<div class="modal-footer">
+														<button type="button" class="btn btn-primary" data-bs-dismiss="modal">Cerrar</button>
+													</div>
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+								
 								<hr class="cart-separator my-2" />
 
-								<div class="d-flex justify-content-between dy-total">
-									<label name="total" for="total">Total: </label>
-									<span id="dy-total">$ --</span>
-								</div>
+								<div>
+									<div class="d-flex justify-content-between dy-subtotal">
+										<label name="subtotal" for="subtotal">Subtotal: </label>
+										<span id="dy-subtotal">$ --</span>
+									</div>
+									<div class="d-flex justify-content-between dy-envio">
+										<label name="envio" for="envio">Envío: </label>
+										<span id="dy-envio">$ --</span>
+									</div>
 
-								<div class="mt-4 d-flex flex-column gap-2 m-auto">
-									<button class="ch-btn-fin">Finalizar compra</button>
-									<button class="ch-btn-back">Continuar navegando</button>
+									<hr class="cart-separator my-2" />
+
+									<div class="d-flex justify-content-between dy-total">
+										<label name="total" for="total">Total: </label>
+										<span id="dy-total">$ --</span>
+									</div>
+
+									<div class="mt-4 d-flex flex-column gap-2 m-auto">
+										<button class="ch-btn-fin" type="submit" form="cart-form">Finalizar compra</button>
+										<button class="ch-btn-back">Continuar navegando</button>
+									</div>
 								</div>
 							</div>
-						</div>
+						</form>
 				</div>
 			</div>
 		`;
